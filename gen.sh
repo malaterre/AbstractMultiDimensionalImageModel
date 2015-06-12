2385522d6336d5742bef54ab3d0b376228576e9b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-06-12 16:11:40 +0200

    update 4D example

---
+++ gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#<DataAt indexWithinDimension="29" descriptorUUID="1fbe0106-6c20-4cf9-b659-ce6185527ace" bulkDataUUID="1b2f1021-b724-4db4-a877-53a6895c5385"/>
+
+for j in {1..10}; do
+  uid0=$(uuidgen -r); 
+  echo "<DataAt indexWithinDimension=\"$j\" descriptorUUID=\"$uid0\">"
+  echo "<DimensionalData dimensionID=\"3\">"
+  for i in {1..64}; do
+    uid1=$(uuidgen -r); 
+    uid2=$(uuidgen -r); 
+    #echo "$uid1 $uid2";
+    echo "<DataAt indexWithinDimension=\"$i\" descriptorUUID=\"$uid1\" bulkDataUUID=\"$uid2\"/>"
+  done
+  echo "</DimensionalData>"
+  echo "</DataAt>"
+done
