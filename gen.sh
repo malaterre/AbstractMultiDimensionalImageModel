45527ba56408d8f3ee5cdf21be79987cfd782e67
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2015-06-12 16:35:07 +0200

    update metabolite single component

--- gen.sh
+++ gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #<DataAt indexWithinDimension="29" descriptorUUID="1fbe0106-6c20-4cf9-b659-ce6185527ace" bulkDataUUID="1b2f1021-b724-4db4-a877-53a6895c5385"/>
 
-for j in {1..10}; do
+for j in {1..64}; do
   uid0=$(uuidgen -r); 
   echo "<DataAt indexWithinDimension=\"$j\" descriptorUUID=\"$uid0\">"
   echo "<DimensionalData dimensionID=\"3\">"
-  for i in {1..64}; do
+  for i in {1..4}; do
     uid1=$(uuidgen -r); 
     uid2=$(uuidgen -r); 
     #echo "$uid1 $uid2";
